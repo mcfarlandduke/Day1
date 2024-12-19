956928ec59019eb85caea7c22d275cf2e98a425b
Duke McFarland <dukeamcfarland@gmail.com>
2024-12-18 19:00:29 -0700

    Fixed Glasses Emoji

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 #I am so lost
 #do I need to have VS Code or just VS?
+
+import streamlit as st
+
+st.set_page_config(page_title="All About Me", page_icon=":sneezing_face:", layout="wide")
+
+# ----HEADER SECTION----
+st.subheader("Hi, I am Duke :wave:")
+st.title("A Struggling College Computer Science Major at UVU")
+st.write("I am making this website to learn about the way I can use Python to make websites for an E2i Project in School :smiling_face_with_sunglasses:")
+st.write("[Learn More >](https://www.youtube.com/watch?v=dQw4w9WgXcQ)")
