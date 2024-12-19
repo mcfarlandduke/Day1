b3e46915ad0faa097dcae5e70da05ff781524cdb
Duke McFarland <dukeamcfarland@gmail.com>
2024-12-18 19:07:53 -0700

    Realized I don't commmit till it works

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ st.set_page_config(page_title="All About Me", page_icon=":sneezing_face:", layou
 # ----HEADER SECTION----
 st.subheader("Hi, I am Duke :wave:")
 st.title("A Struggling College Computer Science Major at UVU")
-st.write("I am making this website to learn about the way I can use Python to make websites for an E2i Project in School", ":smiling_face_with_sunglasses:")
+st.write("I am making this website to learn about the way I can use Python to make websites for an E2i Project in School", ":sunglasses:")
 st.write("[Learn More >](https://www.youtube.com/watch?v=dQw4w9WgXcQ)")
