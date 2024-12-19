067cb6fb1070b4cd707033e72e115edb02647cd6
Duke McFarland <dukeamcfarland@gmail.com>
2024-12-18 21:53:09 -0700

    taking out emojis

--- app.py
+++ app.py
@@ -4,14 +4,14 @@
 from PIL import Image
 import streamlit as st
 
-st.set_page_config(page_title="All About Me", page_icon=":sneezing_face:", layout="wide")
+st.set_page_config(page_title="All About Me", page_icon="##", layout="wide")
 
 img_monkey = Image.open("images/monkey.png")
 img_headshot = Image.open("images/headshot.png")
 
 # ----HEADER SECTION----
 with st.container():
-    st.subheader("Hi, I am Duke :wave:")
+    st.subheader("Hi, I am Duke")
     st.title("A Struggling College Computer Science Major at UVU")
     st.write("I am making this website to learn about the way I can use Python to make websites for an E2i Project in School", ":sunglasses:")
     st.write("[Learn More >](https://www.youtube.com/watch?v=dQw4w9WgXcQ)")
